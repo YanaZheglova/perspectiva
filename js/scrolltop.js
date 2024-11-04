fb20887b37e816cd9c0fc39fcfe31e64c87b35f9
Yana <93075958+YanaZheglova@users.noreply.github.com>
2024-11-04 21:12:12 +0300

    add two page

---
+++ js/scrolltop.js
@@ -0,0 +1,23 @@
+/* scroll top*/
+
+
+const scrollBtn = document.querySelector(".btn");
+
+const btnVisibility = () => {
+    if (window.scrollY > 400) {
+        scrollBtn.style.visibility = "visible";
+    } else {
+        scrollBtn.style.visibility = "hidden";
+    }
+};
+
+document.addEventListener("scroll", () => {
+    btnVisibility();
+});
+
+scrollBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
