f6ebd8b66d4524d84eb72ccf355fc2c806ea3c16
Yana <93075958+YanaZheglova@users.noreply.github.com>
2024-11-08 17:32:51 +0300

    add page about us

---
+++ 404.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Template Name: 404
+*/
+?>
+ <?php get_header('page'); ?>
+
+    <main>
+      <section class="found">
+        <div class="container">
+          <div class="found__row">
+            <h1 class="found__title">
+              4
+              <span> 0 </span>0
+            </h1>
+            <h2 class="found__subtitle">Страница не найдена!</h2>
+            <div class="found__text">
+              Извините, но запрашиваемая вами страница не доступна.
+            </div>
+            <div class="found__button">
+              <a href="https://lab-perspektiva.ru/">Перейти на главную</a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+		<?php get_footer(); ?>
